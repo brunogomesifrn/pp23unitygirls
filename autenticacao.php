60cf58a2b1143b69ad4e0a9b2b594ac2de354c6d
amandagnsc <n.gabriela@escolar.ifrn.edu.br>
2023-12-06 13:52:04 -0300

    atualizei_nomes

---
+++ autenticacao.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en"data-bs-theme="auto">
+  <head><script src="src/js/color-modes.js"></script>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Hugo 0.118.2">
+    <title>Carousel Template · Bootstrap v5.3</title>
+    <link rel="canonical" href="https://getbootstrap.com/docs/5.3/examples/carousel/">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@docsearch/css@3">
+<link href="src/css/bootstrap.min.css" rel="stylesheet">
+
+  <style>
+    }
+.rodape{
+    padding-bottom: 20px;
+    text-align: center
+    }
+footer {
+	background-color: #a070df;
+	color: #fff;
+  padding: 20px;
+	text-align: center;
+  margin-top: 50px;
+}
+ul {
+	list-style: none;
+	padding: 0;
+	margin: 0;
+}
+li {
+	display: inline-block;
+	margin: 0 10px;
+}
+a {
+	color: #fff;
+	text-decoration: none;
+}
+</style>
+<body>
+   
+    <?php include 'bases/menu.php' ?>
+    <div class="container mt-5">
+
+   <div class="row mt-5 mb-5">
+   <div class="col-lg-12">
+
+<div class="form">
+    <center>
+        <h1>Autenticação:</h1>    
+    <form action="bd_autenticacao.php" method="post">
+        <p>
+            <label for="nome">Nome:</label>
+            <input type="text" name="nome" id="nome"/>
+        </p>
+        <p>
+            <label for="senha">Senha:</label>
+            <input type="password" name="senha" id="senha"/>
+        </p>
+</form>
+</div>
+</div>
+</div>
+       
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+</center>
+<?php include 'bases/rodape.php' ?>
+  <script src="src/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
