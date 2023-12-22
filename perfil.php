fc12faffc4ee572583ab935a0d5bf636a688d352
BeaSant0s <beatrizcavalcante576@gmail.com>
2023-12-22 14:53:27 -0300

    atualização do usuário

--- perfil.php
+++ perfil.php
@@ -1,3 +1,10 @@
+<?php 
+          // Verificar se sessão já foi iniciada anteriormentre
+          if(session_id() == '')
+            session_start();
+          
+     
+          ?>
 <!doctype html>
 <html lang="en" data-bs-theme="auto">
   <head>
